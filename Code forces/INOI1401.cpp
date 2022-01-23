a60ab34a92a7bad3d34b4d566845936ac7a53914
amro-coder <AmroAdil420@gmail.com>
2022-01-23 14:24:51 +0000

    DP problem

--- Code forces/INOI1401.cpp
+++ Code forces/INOI1401.cpp
@@ -31,16 +31,17 @@ int main() {
     for(int i=0;i<=d;i++)
         dp[r - 1][c-1][i][0] = 1;
     for (int i=r-1;i>=0;i--)
-        for(int j=c-1;j>=0;j--){
-            if(grid[i][j]==0 || ((i==r-1)&&(j==c-1)))
+        for(int j=c-1;j>=0;j--) {
+            if (grid[i][j] == 0 || ((i == r - 1) && (j == c - 1)))
                 continue;
-            else
-//          if we go right
-                for (int right_moves_left=1;right_moves_left<=d;right_moves_left++)
-                    dp[i][j][right_moves_left][0]=(dp[i][j+1][right_moves_left-1][0]%20011+dp[i][j+1][d][1]%20011)%20011;
-//          if we go down
-            for(int down_moves_left=1;down_moves_left<=d;down_moves_left++)
-                dp[i][j][down_moves_left][1]=(dp[i+1][j][down_moves_left-1][1]%20011+dp[i+1][j][d][0]%20011)%20011;
+            for (int moves_left = 1; moves_left <= d; moves_left++) {
+                //          if we go right
+                dp[i][j][moves_left][0] =
+                        (dp[i][j + 1][moves_left - 1][0] % 20011 + dp[i][j + 1][d][1] % 20011) % 20011;
+                //          if we go down
+                dp[i][j][moves_left][1] =
+                        (dp[i + 1][j][moves_left - 1][1] % 20011 + dp[i + 1][j][d][0] % 20011) % 20011;
+            }
         }
     cout<<(dp[0][0][d][0]+dp[0][0][d][1])%20011<<'\n';
 
