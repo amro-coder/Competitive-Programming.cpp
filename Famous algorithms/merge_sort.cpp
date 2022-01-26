d12eaa1f527c343ddf1c4cc396b27c9d35be4ff5
amro-coder <AmroAdil420@gmail.com>
2022-01-26 08:28:00 +0000

    used defined merge function

--- Famous algorithms/merge_sort.cpp
+++ Famous algorithms/merge_sort.cpp
@@ -8,31 +8,6 @@ void print(vector<int> x){
     for(auto i:x)
         cout<<i<<' ';
 }
-vi merge(vi x,vi y){
-    vi sorted;
-    auto elex=x.begin();
-    auto eley=y.begin();
-    while (true) {
-        if(elex==x.end()){
-            sorted.insert(sorted.end(),eley,y.end());
-            break;
-        }
-        if(eley==y.end()){
-            sorted.insert(sorted.end(),elex,x.end());
-            break;
-        }
-        if (*elex > *eley) {
-            sorted.insert(sorted.end(), *eley);
-            eley++;
-        }
-        else{
-            sorted.insert(sorted.end(), *elex);
-            elex++;
-        }
-
-    }
-    return sorted;
-}
 vi merge_sort(vi x){
     int n=x.size();
     if(n<=1)
@@ -43,23 +18,17 @@ vi merge_sort(vi x){
     x.resize(n/2);
     vi left= merge_sort(x);
     vi right= merge_sort(y);
-    return merge(left,right);
+    vi sorted(n);
+    merge(left.begin(),left .end(),right.begin(),right.end(),sorted.begin());
+    return sorted;
 }
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int n;
     cin>>n;
-    vi x;
-    for (int i=0,temp;i<n;i++){
-        cin>>temp;
-        x.pb(temp);
-    }
+    vi x(n);
+    for(auto &i:x)cin>>i;
     print(merge_sort(x));
     return 0;
 }
-
-
-
-
-
