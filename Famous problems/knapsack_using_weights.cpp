8373feef0fc3a5861087ecf5d392c1ea674e1dc7
amro-coder <AmroAdil420@gmail.com>
2022-02-02 13:08:22 +0000

    O(w) space!!

--- Famous problems/knapsack_using_weights.cpp
+++ Famous problems/knapsack_using_weights.cpp
@@ -5,6 +5,7 @@ typedef long long ll ;
 typedef long double ld;
 typedef vector<int> vi;
 typedef vector<ll> vl;
+typedef vector<bool> vb;
 typedef pair<int,int> pi;
 
 
@@ -13,9 +14,10 @@ typedef pair<int,int> pi;
 #define PB push_back
 #define MP make_pair
 
-void print(vector<int> x){
+void print(vl x){
     for(auto i:x)
         cout<<i<<' ';
+    cout<<'\n';
 }
 
 int main() {
@@ -23,31 +25,36 @@ int main() {
     cin.tie(0);
     int n,w;
     cin>>n>>w;
-    vi value(n+1);
-    vi weight(n+1);
-    for(int i=1;i<n+1;i++){
+    vi value(n);
+    vi weight(n);
+    for(int i=0;i<n;i++){
         cin>>weight[i];
         cin>>value[i];
     }
-    ll max_value=n*(*max_element(value.begin()+1, value.end()));
-    vector<vl> dp(n+1,(vl (max_value+1, (ll)pow(10,11))));
-    for (int i=0;i<=n;i++)
-        dp[i][0]=0;
-
-    for (int i=1;i<=n;i++)
-        for(int j=1;j<=max_value;j++){
-            dp[i][j]=dp[i-1][j];
-            if(j-value[i]>=0)
-                dp[i][j]=min(dp[i][j],weight[i]+dp[i-1][j-value[i]]);
-        }
-    int ans;
-    for (int i=max_value;i>=0;i--)
-        if(dp[n][i]<=w){
-            ans=i;
-            break;
-        }
+    int max_value=n*(*max_element(value.begin(), value.end()));
+    vl dp(max_value+1, LONG_LONG_MAX/2);
+    dp[0]=0;
+    for (int i=0;i<n;i++)
+        for (int j=max_value;j>=value[i];j--)
+            dp[j]=min(dp[j],dp[j-value[i]]+weight[i]);
+    int ans=max_value-(find_if(dp.rbegin(),dp.rend(),[w](ll x){return x<=w;})-dp.rbegin());
     cout<<ans<<'\n';
+//  reconstruct solution:
+    vector<pi> solution;
+    int i =ans;
+    vb visited(n);
+    while (i>0) {
+        for (int j = 0; j < n; j++)
+            if ((value[j] <= i) && (!visited[j]) && (dp[i] == dp[i - value[j]] + weight[j])) {
+                solution.PB(MP(weight[j], value[j]));
+                i -= value[j];
+                visited[j] = true;
+                break;
+            }}
+    for (auto i: solution)
+        cout << i.F << ' ' << i.S << '\n';
     return 0;
+
 }
 
 
