21104b1fb72e762c5dda5acfa391a4a805ed9ee1
amro-coder <AmroAdil420@gmail.com>
2022-02-02 13:07:51 +0000

    O(w) space!!

--- Famous problems/knapsack_problem.cpp
+++ Famous problems/knapsack_problem.cpp
@@ -4,6 +4,7 @@ using namespace std;
 typedef long long ll ;
 typedef long double ld;
 typedef vector<int> vi;
+typedef vector<bool> vb;
 typedef pair<int,int> pi;
 typedef vector<ll> vl;
 
@@ -12,7 +13,7 @@ typedef vector<ll> vl;
 #define S second
 #define PB push_back
 #define MP make_pair
-void print(vector<int> x){
+void print(vl x){
     for(auto i:x)
         cout<<i<<' ';
     cout<<'\n';
@@ -22,33 +23,35 @@ int main() {
     cin.tie(0);
     int n,w;
     cin>>n>>w;
-    vi values(n+1),weights(n+1);
-    for (int i=1;i<=n;i++){
+    vi values(n),weights(n);
+    for (int i=0;i<n;i++){
         cin>>weights[i];
         cin>>values[i];
     }
-    vector<vl> dp(n+1,vl(w+1));
-    for (int i=1;i<=n;i++)
-        for(int j=1;j<=w;j++){
-            dp[i][j]=dp[i-1][j];
-            if(weights[i]<=j){
-                dp[i][j]=max(dp[i][j],values[i]+dp[i-1][j-weights[i]]);
+    vl dp(w+1);
+    for (int i=0;i<n;i++)
+        for (int j=w;j>=weights[i];j--)
+            dp[j]=max(dp[j],dp[j-weights[i]]+values[i]);
+    cout<<dp[w]<<'\n';
+    // reconstructing solution
+    vector<pi> ans;
+    int i =w;
+    vb visited(n);
+    while (i>0){
+        bool took_element= false;
+        for (int j=0;j<n;j++)
+            if((weights[j]<=i)&&(!visited[j])&&(dp[i]==dp[i-weights[j]]+values[j])){
+                ans.PB(MP(weights[j], values[j]));
+                i -= weights[j];
+                visited[j]=true;
+                took_element= true;
+                break;
             }
-        }
-    cout<<dp[n][w]<<'\n';
-//  reconstructing solution
-    vector<pi> knapsack;
-    int i=n,j=w;
-    while (i>0 && j>0){
-        if(dp[i][j]!=dp[i-1][j]){
-            knapsack.PB(MP(weights[i],values[i]));
-            j-=weights[i];
-        }
-        i-=1;
+        if(!took_element)
+            i-=1;
     }
-    for (auto i:knapsack)
+    for(auto i:ans)
         cout<<i.F<<' '<<i.S<<'\n';
-
     return 0;
 }
 
