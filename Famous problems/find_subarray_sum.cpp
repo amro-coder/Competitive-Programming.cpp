59a82ba9cd99dc2dc28c5a9cafc4500321484de5
amro-coder <AmroAdil420@gmail.com>
2022-02-03 09:49:07 +0000

    O(n)

---
+++ Famous problems/find_subarray_sum.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll ;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef pair<int,int> pi;
+
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+void print(vector<int> x){
+    for(auto i:x)
+        cout<<i<<' ';
+    cout<<'\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n,target;cin>>n>>target;
+    vi x(n);
+    for (auto &i:x)cin>>i;
+    int i=0,j=0,sum=0;
+    bool ans=false;
+    while(i<n && j<n)
+    {
+        if(sum==target){
+            ans= true;
+            break;
+        } else
+        if(sum<target){
+            sum+=x[j];
+            j++;
+        } else {
+            sum -= x[i];
+            i++;
+        }
+    }
+    if(ans)
+        cout<<"sum found"<<" from index "<<i+1<<" to index "<<j<<'\n';
+    else
+        cout<<"sum not found"<<'\n';
+    return 0;
+}
+
+
+
+
+
+//
+// Created by amroa on 03/02/2022.
+//
+
