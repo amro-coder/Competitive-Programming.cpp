a53fcf5d3416098dd28dd6819767869962aa012b
amro-coder <AmroAdil420@gmail.com>
2022-02-11 19:10:40 +0000

    using dfs

--- Graph/connected_components.cpp
+++ Graph/connected_components.cpp
@@ -16,6 +16,24 @@ typedef vector<pi> vp;
 #define PB push_back
 #define MP make_pair
 
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll ;
+typedef long double ld;
+typedef pair<int,int> pi;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef vector<pi> vp;
+
+
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
 void print(vi x){
     for(auto i:x)
         cout<<i<<' ';
@@ -24,22 +42,7 @@ void print(vi x){
 
 //graph with no weights
 vector<vi> graph;
-void reset_graph_to_size(int n){
-    graph.resize(n+1);
-    for(int i=0;i<n;i++)
-        graph[i].resize(0);
-    return;
-}
-
-void add_edge_both_directions(int a,int b){
-    graph[a].PB(b);
-    graph[b].PB(a);
-}
-vb create_visited(){
-    int n=graph.size();
-    vb visited(n);
-    return visited;
-}
+int n,m;
 vb visited;
 void DFS(int node){
     vi stack={node};
@@ -48,7 +51,9 @@ void DFS(int node){
         stack.pop_back();
         if(!visited[parent]){
             visited[parent]=true;
-            for (auto child:graph[parent]) stack.PB(child);
+            for (auto child:graph[parent])
+                if(!visited[child])
+                    stack.PB(child);
         }
     }
     return;
@@ -58,22 +63,30 @@ void DFS(int node){
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int n,m;cin>>n>>m;
-    reset_graph_to_size(n);
+    cin>>n>>m;
+    graph.resize(n+1);
     for(int i=0;i<m;i++){
         int a,b;cin>>a>>b;
-        add_edge_both_directions(a,b);
+        graph[a].PB(b);
+        graph[b].PB(a);
     }
+    visited.resize(n+1);
     int connected_components=0;
-    visited=create_visited();
-    for(int i=0;i<n;i++){
+    for (int i=0;i<n;i++)
         if(!visited[i]){
-            connected_components++;
             DFS(i);
+            connected_components++;
         }
-    }
-    cout<<connected_components;
-
-
+    cout<<"number of connected components= "<<connected_components<<'\n';
     return 0;
 }
+//example input:
+//10 8
+//0 1
+//0 3
+//1 4
+//5 7
+//3 6
+//2 8
+//8 9
+//9 10
