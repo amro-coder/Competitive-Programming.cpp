42a4105b780067727ca753937da71a97375eabea
amro-coder <AmroAdil420@gmail.com>
2022-03-09 16:26:02 +0000

    using DFS

---
+++ Graph/Tree algorithms/contained_nodes_in_sub_tree.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll ;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+typedef pair<int,int> pi;
+typedef tuple<int,int,int> tup;
+
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+const ll INF=(1LL<<60);
+
+
+// debuging tools
+void print(vl x) {
+    for (auto i: x)
+        cout << i << ' ';
+    cout << '\n';
+}
+void print(vi x) {
+    for (auto i: x)
+        cout << i << ' ';
+    cout << '\n';
+}
+void print_pair(pi x){
+    cout<<x.F<<" "<<x.S<<"\n";
+}
+void print2D(vector<vi> x){
+    for (auto i:x)
+        print(i);
+    cout<<"\n";
+}
+void print2D(vector<vl> x){
+    for (auto i:x)
+        print(i);
+    cout<<"\n";
+}
+// my code
+
+vector<vi> graph;
+vi contained_nodes_in_sub_tree;
+void dfs_contained_nodes_in_sub_tree(int node,int parent){
+//  calucluate contained nodes to my children
+    int ans=1;//node is containing it's self
+    for (auto child:graph[node])
+        if (child!=parent) {
+            dfs_contained_nodes_in_sub_tree(child, node);
+            ans+=contained_nodes_in_sub_tree[child];
+        }
+//  use my children values to caluclate my answer
+    contained_nodes_in_sub_tree[node]=ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n; cin>>n;
+    graph.resize(n);
+    contained_nodes_in_sub_tree.resize(n);
+    for (int i=0;i<n-1;i++){
+        int a,b;cin>>a>>b;
+        graph[a].PB(b);
+        graph[b].PB(a);
+    }
+    dfs_contained_nodes_in_sub_tree(0,-1);
+    print(contained_nodes_in_sub_tree);
+    return 0;
+}
+
+
+
+
+//10
+//0 1
+//1 2
+//2 3
+//2 4
+//0 5
+//5 6
+//5 8
+//6 7
+//7 9
+
